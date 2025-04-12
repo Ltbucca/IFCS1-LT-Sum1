cd45b0752f3061ae1de8d2d477cb0f093d14bc49
Luke Tomlinson <1.luke.tomlinson@gmail.com>
2025-04-12 12:24:44 +0100

    Update file and ignore .vscode folder

--- Summative-1-Task-3/test_fizzbuzz.py
+++ Summative-1-Task-3/test_fizzbuzz.py
@@ -2,8 +2,8 @@ import unittest
 from fizzbuzz import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
-    # def test_tests_work(self):
-    #     self.assertEqual(2+2,4)
+    def test_tests_work(self):
+        self.assertEqual(2+2,4)
 
     def test_fizzbuzz(self):
         self.assertEqual(fizzbuzz(15), [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz'])
